5ae22e43f8040a4913f47efb3c188a29238ba9a6
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-05-31 21:35:05 +0530

    Insertion and saving in database using annotations

--- src/main/java/com/spring/hibernate/App.java
+++ src/main/java/com/spring/hibernate/App.java
@@ -1,7 +1,10 @@
 package com.spring.hibernate;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -11,10 +14,27 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Started" );
+        
         Configuration cfg = new Configuration();
         cfg.configure("hibernate.cfg.xml");
-        
         SessionFactory factory = cfg.buildSessionFactory();
+        
+        // creating Student
+        Student st = new Student();
+        st.setId(2);
+        st.setName("Aman");
+        st.setCity("Banglore");
+        
+        System.out.println(st);
+        
+        Session session = factory.getCurrentSession();
+        
+        Transaction tx = session.beginTransaction();
+        session.save(st);
+        tx.commit();
+        
+        session.close();
+        
         System.out.println(factory);
         System.out.println(factory.isClosed());
     }
---
+++ src/main/java/com/spring/hibernate/Student.java
@@ -0,0 +1,57 @@
+package com.spring.hibernate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity		// to change name => @Entity(name="")
+//@Table(name="")		// to keep the entity name as Student but table name as something else
+public class Student {
+	@Id		// to denote primary key
+	private int id;
+	
+	private String name;
+	private String city;
+	
+	public Student(int id, String name, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+	}
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+}
