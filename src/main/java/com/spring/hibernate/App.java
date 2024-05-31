3695a0cbe6f30348e0daf7d38b29d522a00982d0
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-05-31 20:36:40 +0530

    Hibernate configuration | Connection with database

--- src/main/java/com/spring/hibernate/App.java
+++ src/main/java/com/spring/hibernate/App.java
@@ -1,5 +1,7 @@
 package com.spring.hibernate;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 /**
  * Hello world!
  *
@@ -8,6 +10,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Started" );
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        
+        SessionFactory factory = cfg.buildSessionFactory();
+        System.out.println(factory);
+        System.out.println(factory.isClosed());
     }
 }
