dd75c3163f3eebdbc7ac58437f15bcd56620fbd9
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-06-01 08:47:26 +0530

    Saving image and other values

--- src/main/java/com/spring/hibernate/App.java
+++ src/main/java/com/spring/hibernate/App.java
@@ -1,5 +1,9 @@
 package com.spring.hibernate;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -19,20 +23,50 @@ public class App
         cfg.configure("hibernate.cfg.xml");
         SessionFactory factory = cfg.buildSessionFactory();
         
-        // creating Student
+        // creating object of Student class
         Student st = new Student();
-        st.setId(2);
+        st.setId(1);
         st.setName("Aman");
         st.setCity("Banglore");
-        
         System.out.println(st);
         
-        Session session = factory.getCurrentSession();
+        // creating object of Address class
+        Address address = new Address();
+        address.setStreet("street1");
+        address.setCity("NOIDA");
+        address.setOpen(true);
+        address.setAddedDate(new Date());
+        address.setX(1.1);
         
+        // reading image
+        try {
+        	File file = new File("src/main/java/pic.png");
+            if (!file.exists()) {
+                System.err.println("File does not exist ");
+                System.err.println("Absolute Path: " + file.getAbsolutePath());
+            }
+            
+        	FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+        	byte[] data = new byte[fis.available()];
+        	fis.read(data);
+        	System.out.println(data);
+        	
+        	try {        		
+        		address.setImage(data);
+        	}
+        	catch(Exception e) {
+        		System.out.println(e.getMessage());
+        	}
+        }
+        catch(Exception e) {
+        	System.out.println(e.getMessage());
+        }
+        
+        Session session = factory.openSession();
         Transaction tx = session.beginTransaction();
         session.save(st);
+        session.save(address);
         tx.commit();
-        
         session.close();
         
         System.out.println(factory);
