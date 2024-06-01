d0e7314b67a14ab6d2e5768e06844874dde4dba6
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-06-01 18:05:53 +0530

    Many to Many mapping

---
+++ src/main/java/com/mapping/manytomany/Emp.java
@@ -0,0 +1,61 @@
+package com.mapping.manytomany;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Emp {
+	@Id
+	private int eid;
+
+	private String name;
+
+	@ManyToMany
+	@JoinTable(name="empAndProjectConnected",		 // this is a join column (the primary key of this table will be connected by primary key o the Project table (will act as foreign key))
+		joinColumns = { @JoinColumn(name="eid") } ,		// the first column of the join table
+		inverseJoinColumns = { @JoinColumn(name="pid") }		// the second column of the join table
+	)
+	private List<Project> projects;
+
+	public Emp(int eid, String name, List<Project> projects) {
+		super();
+		this.eid = eid;
+		this.name = name;
+		this.projects = projects;
+	}
+
+	public Emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Project> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+
+}
---
+++ src/main/java/com/mapping/manytomany/MappingDemo.java
@@ -0,0 +1,63 @@
+package com.mapping.manytomany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+public class MappingDemo {
+
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        
+        Emp e1 = new Emp();
+        Emp e2 = new Emp();
+        
+        e1.setEid(1);
+        e1.setName("Aman");
+
+        e2.setEid(2);
+        e2.setName("Shyam");
+        
+        Project p1 = new Project();
+        Project p2 = new Project();
+        
+        p1.setPid(10);
+        p1.setProjectName("Tumble");
+        
+        p2.setPid(11);
+        p2.setProjectName("Cluster");
+        
+        List<Emp> list1 = new ArrayList<Emp>();
+        List<Project> list2 = new ArrayList<Project>();
+        
+        list1.add(e1);
+        list1.add(e2);
+        
+        list2.add(p1);
+        list2.add(p2);
+        
+        e1.setProjects(list2);
+        p2.setEmps(list1);
+        
+        Session s = factory.openSession();
+        Transaction tx = s.beginTransaction();
+        
+        s.save(e1);
+        s.save(e2);
+        s.save(p1);
+        s.save(p2);
+        
+        tx.commit();
+        s.close();
+        
+        factory.close();
+	}
+
+}
